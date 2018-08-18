a8331937b2eb82f646e19745dc29c9868142fdeb
wuchuanchuan <superwucc@gmail.com>
2018-08-18 06:54:55 +0000

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="http://www.miour.cn/index.html" target="_blank">上海米澳网络科技有限公司</a> 提供技术支持
+        </p>
+        <p class="pull-right"><a href="joe@miour.cn">联系我们</a></p>
+    </div>
+</footer>
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title', '建军工程大学试卷题库')
+
+@section('content')
+    <h1>建军工程大学试卷题库</h1>
+@stop
