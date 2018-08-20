e13605eea152312a5a39d7ff656fe74577801a2c
wuchuanchuan <superwucc@gmail.com>
2018-08-20 05:20:36 +0000

    Add subject model and generator

---
+++ database/migrations/2018_08_20_130959_create_subjects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('bn')->index()->comment('编号');
+            $table->string('name')->index()->comment('名称');
+            $table->integer('subject_hour')->default(0)->comment('课时');
+            $table->text('introduction')->nullable()->comment('简介');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subjects');
+    }
+}
