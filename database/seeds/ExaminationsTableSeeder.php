101edd0868c542ad6d0ade9fc2eb4af3ce7c4971
wuchuanchuan <superwucc@gmail.com>
2018-08-20 07:55:42 +0000

    Add permission

--- database/seeds/ExaminationsTableSeeder.php
+++ database/seeds/ExaminationsTableSeeder.php
@@ -7,13 +7,7 @@ class ExaminationsTableSeeder extends Seeder
 {
     public function run()
     {
-        $examinations = factory(Examination::class)->times(50)->make()->each(function ($examination, $index) {
-            if ($index == 0) {
-                // $examination->field = 'value';
-            }
-        });
 
-        Examination::insert($examinations->toArray());
     }
 
 }
